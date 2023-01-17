cd364827f94e84738b213703e2b0a5af43fcbab1
Timm Schoenborn <timmschoenborn@gmail.com>
2023-01-17 09:53:31 -0600

    server updated

---
+++ server.js
@@ -0,0 +1,45 @@
+/////////////////////////////////////
+//// Import Dependencies         ////
+/////////////////////////////////////
+const express = require('express') // import the express framework
+const mongoose = require('mongoose') // import the mongoose library
+const morgan = require('morgan') // import the morgan request logger
+require('dotenv').config() // Load my ENV file's variables
+
+/////////////////////////////////////
+//// Database Connection         ////
+/////////////////////////////////////
+// this is where we will set up our inputs for our database connect function
+const DATABASE_URL = process.env.DATABASE_URL
+// here is our DB config object
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+// establish our database connection
+mongoose.connect(DATABASE_URL, CONFIG)
+
+// Tell mongoose what to do with certain events
+// what happens when we open, diconnect, or get an error
+mongoose.connection
+    .on('open', () => console.log('Connected to Mongoose'))
+    .on('close', () => console.log('Disconnected from Mongoose'))
+    .on('error', (err) => console.log('An error occurred: \n', err))
+
+
+/////////////////////////////////////
+//// Create our Express App Object //
+/////////////////////////////////////
+const app = express()
+
+
+
+
+/////////////////////////////////////
+//// Server Listener             ////
+/////////////////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now listening to the sweet sounds of port: ${PORT}`))
+
+// END

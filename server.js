6da7745ba99fbf69774b0f1c0b14980c7bb15a1b
Timm Schoenborn <timmschoenborn@gmail.com>
2023-01-19 13:16:55 -0600

    already set up with liquid ready for templating

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ const middleware = require('./utils/middleware')
 /////////////////////////////////////
 //// Create our Express App Object //
 /////////////////////////////////////
-const app = express()
+// this was fine for building an API that sends and receives json
+// const app = express()
+// but now, our app is going to be Full-Stack. That means handling front-end and back-end from the same server(in this case).
+// so, we're utilizing an npm package `liquid-express-views` to add the 'view' layer to our MVC framework.
+// in short, we need to update our app object and tell it to use that package, as stated by the documentation. 
+const app = require('liquid-express-views')(express())
+// what liquid-express-views really does for us, is make it easy to path to our .liquid files(which will serve our html). This package says to look inside the 'views' folder for files with the .liquid name.
 
 /////////////////////////////////////
 //// Middleware                  ////
@@ -27,8 +33,9 @@ middleware(app)
 /////////////////////////////////////
 //// Routes                      ////
 /////////////////////////////////////
+// HOME route
 app.get('/', (req, res) => {
-    res.send('Server is live, ready for requests')
+    res.render('home.liquid')
 })
 
 // This is now where we register our routes, this is how server.js knows to send the correc response. 
--- utils/middleware.js
+++ utils/middleware.js
@@ -6,6 +6,7 @@ const morgan = require('morgan') // import the morgan request logger
 const session = require('express-session') // import the express-session package
 const MongoStore = require('connect-mongo') // import the connect-mongo package(for sessions)
 require('dotenv').config()
+const methodOverride = require('method-override')
 
 /////////////////////////////////////
 //// Middleware function         ////
@@ -14,8 +15,13 @@ require('dotenv').config()
 const middleware = (app) => {
     // middleware runs before all the routes.
     // every request is processed through our middleware before mongoose can do anything with it
+    // method-override is middleware that allows us to utilize forms to their full potential.
+    // by default, forms can ONLY and I mean ONLY send a GET or a POST request.
+    // method-override allows us to send PUT, PATCH, DELETE, and other requests from a form, by defining it with '_method'
+    app.use(methodOverride('_method'))
     app.use(morgan('tiny')) // this is for request loggging, the 'tiny' argument declares what size of morgan log to use
     app.use(express.urlencoded({ extended: true })) //this parses urlEncoded request bodies(useful for POST and PUT requests)
+    // using express.static('public') allows us to serve a single CSS stylesheet across our application where we want to.
     app.use(express.static('public')) // this serves static files from the 'public' folder
     app.use(express.json()) // parses incoming request payloads with JSON
     // here, we set up and utilize a session function, and we pass that function a config argument, to configure our session in the way we want. This argument will tell express-session how to create and store our session.
